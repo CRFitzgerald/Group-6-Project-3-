240b10036b98df686938606be9a65d56fa0315cb
minkyong1203 <minkyongkim3@gmail.com>
2023-04-05 21:57:12 -0700

    changed scraping script and index.html. created new output file

--- joel_main.js
+++ joel_main.js
@@ -1,6 +1,6 @@
 // Code block for the bar chart of top 10 rating and review count
 // Load the JSON file
-$.getJSON('top_100_books.json', function(data) {
+$.getJSON('top100_books_2022.json', function(data) {
     console.log(data);
 
     // Set up the chart options
@@ -63,12 +63,12 @@ $.getJSON('top_100_books.json', function(data) {
   
     // Render the initial chart
     renderChart(data, 'rating', options);
-  
+
   });
 
 // Code block for selecting the book
 // Load the JSON file
-d3.json('top_100_books.json').then(function(data) {
+d3.json('top100_books_2022.json').then(function(data) {
   // Sort the data by title in alphabetical order
   data.sort(function(a, b) {
       var titleA = a.title.toUpperCase(); // ignore upper and lowercase
@@ -103,20 +103,76 @@ d3.json('top_100_books.json').then(function(data) {
   d3.select('#hardcover-price').text(firstBook.prices.Hardcover);
   d3.select('#paperback-price').text(firstBook.prices.Paperback);
 
-  // Display the book information when a title is selected
-  dropdown.on('change', function() {
-      var selectedTitle = this.value;
-      var selectedBook = data.find(function(d) {
-          return d.title === selectedTitle;
-      });
 
-      // Update the HTML with the selected book's image, author, publish date, and prices
-      d3.select('#book-image').attr('src', selectedBook.image);
-      d3.select('#book-author').text(selectedBook.author);
-      d3.select('#book-publish-date').text(selectedBook.publish_date);
-      d3.select('#kindle-price').text(selectedBook.prices.Kindle);
-      d3.select('#audiobook-price').text(selectedBook.prices.Audiobook);
-      d3.select('#hardcover-price').text(selectedBook.prices.Hardcover);
-      d3.select('#paperback-price').text(selectedBook.prices.Paperback);
+
+  // Display the book information when a title is selected
+  // dropdown.on('change', function() {
+  //     var selectedTitle = this.value;
+  //     var selectedBook = data.find(function(d) {
+  //         return d.title === selectedTitle;
+  //     });
+
+  //     // Update the HTML with the selected book's image, author, publish date, and prices
+  //     d3.select('#book-image').attr('src', selectedBook.image);
+  //     d3.select('#book-author').text(selectedBook.author);
+  //     d3.select('#book-publish-date').text(selectedBook.publish_date);
+  //     d3.select('#kindle-price').text(selectedBook.prices.Kindle);
+  //     d3.select('#audiobook-price').text(selectedBook.prices.Audiobook);
+  //     d3.select('#hardcover-price').text(selectedBook.prices.Hardcover);
+  //     d3.select('#paperback-price').text(selectedBook.prices.Paperback);
+  // });
+});
+
+d3.json('top100_books_2022.json').then(function(data) {
+  var dropdown = d3.select('#book-select');
+  dropdown.on('change', function() {
+    var selectedTitle = this.value;
+    var selectedBook = data.find(function(d) {
+        return d.title === selectedTitle;
+    });
+    console.log(selectedBook)
+
+    d3.select('#book-image').attr('src', selectedBook.image);
+    d3.select('#book-author').text(selectedBook.author);
+    d3.select('#book-publish-date').text(selectedBook.publish_date);
+    d3.select('#kindle-price').text(selectedBook.prices.Kindle);
+    d3.select('#audiobook-price').text(selectedBook.prices.Audiobook);
+    d3.select('#hardcover-price').text(selectedBook.prices.Hardcover);
+    d3.select('#paperback-price').text(selectedBook.prices.Paperback);
+
+    // join all the reviews together to create one blob of text
+    book_reviews = selectedBook.reviews.join(' '); 
+
+    // getting rid of all the punctuation characters
+    var lines = book_reviews.split(/[,\. ]+/g); 
+
+    var stopwords = new Set (['i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now']);
+    var excluded = lines.filter(word => !stopwords.has(word.toLowerCase()));
+    var clean_data = Highcharts.reduce(excluded, function(arr, word) {
+      var obj = Highcharts.find(arr, function (obj) {
+        return obj.name === word;
+      });
+      if (obj) {
+        obj.weight += 1;
+      } else {
+        arr.push({
+          name: word,
+          weight: 1
+        });
+      }
+      return arr;
+    }, []);
+
+    // Update the wordcloud
+    Highcharts.chart('wordcloud', {
+      series: [{
+        type: 'wordcloud',
+        data: clean_data,
+        name: 'Occurences'
+      }], 
+      title: {
+        text: 'Wordcloud of ' + selectedTitle
+      }
+    });
   });
 });
